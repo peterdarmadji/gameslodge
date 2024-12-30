1ab4385a7803f762af5181baebece3aa546cc05a
peterdarmadji <peterd0608@gmail.com>
2024-12-31 00:57:46 +1100

    Built responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Button } from "@chakra-ui/react"
+import { Button, Grid, GridItem } from "@chakra-ui/react"
 
 function App() {
 
   return (
-    <div>
-      <Button colorPalette={"blue"}>Button</Button>
-    </div>
+    // | nav   | nav  |
+    // | aside | main |
+    <Grid templateAreas={{
+      base: `"nav" "main"`, // mobile devices (< 1024 px)
+      lg: `"nav nav" "aside main"` // 1024 px
+    }}> 
+      {/* Top bar navigation panel */}
+      <GridItem area='nav' bg='coral'>Nav</GridItem>
+      {/* Side panel - only show for 1024 px */}
+      <GridItem area='aside' bg='gold'display={{base: "none", lg: "block"}}>Aside</GridItem>
+      {/* Main contents area */}
+      <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+    </Grid>
   )
 }
 
