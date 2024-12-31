1c2efadf96d9a15e550ef0be31dfa87ad345c3b6
peterdarmadji <peterd0608@gmail.com>
2024-12-31 13:05:07 +1100

    Displaying game platform icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -11,45 +11,22 @@ interface Props {
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
-  // Create an array of all slugs from the platforms
-  const allSlugs = platforms.map((p) => p.slug);
-
-  // Create a unique set of slugs
-  const uniqueSlugs = [...new Set(allSlugs)];
-
-  // Log them to see if there's any unexpected or missing slug
-  console.log("Unique slugs in PlatformIconList:", uniqueSlugs);
-
   const iconMap: {[key: string]: IconType} = {
-    pc: BsGlobe,
-    playstation: BsGlobe,
-    xbox: BsGlobe,
-    mac: BsGlobe,
-    linux: BsGlobe,
-    android: BsGlobe,
-    ios: BsGlobe,
-    nintendo: BsGlobe,
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    nintendo: SiNintendo,
     web: BsGlobe,
   }
 
   return (
-    // <HStack>
-    //   {/* {platforms.map((p) => {
-    //     // 2) Log each individual platform
-    //     console.log("Platform object:", p);
-
-    //     // 3) Log the slug-to-icon lookup
-    //     console.log("Slug:", p.slug, " => Icon:", iconMap[p.slug]);
-
-    //     return <Icon key={p.id} as={iconMap[p.slug] || BsGlobe} />
-    //   }
-    //   )} */}
-
-    //   {/* {platforms.map((platform) => (<Icon as={iconMap[platform.slug]}></Icon>))} */}
-    // </HStack>
-    <div>
-      <Icon as={BsGlobe} boxSize="2rem" color="blue" />
-    </div>
+    <HStack marginY={'10px'}>
+      {platforms.map((platform) => (<Icon as={iconMap[platform.slug]} color={'gray.400'}></Icon>))}
+    </HStack>
   );  
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from './components/ui/provider'
 import App from './App'
 import theme from './theme'
 import './index.css'
-import { ColorModeProvider } from './components/ui/color-mode'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
