491b86181904598b90a5fe85a1adaf6d74b64081
peterdarmadji <peterd0608@gmail.com>
2025-01-02 22:05:53 +1100

    Implemented buggy platform fetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import GenreList from "./components/GenreList"
 import { useState } from "react"
 import { Genre } from "./hooks/use-genres"
 import PlatformSelector from "./components/PlatformSelector"
+import { Platform } from "./hooks/use-games"
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
 
   return (
     // | nav   | nav  |
@@ -33,8 +35,8 @@ function App() {
       </Show>
       {/* Main contents area */}
       <GridItem area='main'>
-        <PlatformSelector></PlatformSelector>
-        <GameGrid selectedGenre={selectedGenre}></GameGrid>
+        <PlatformSelector selectedPlatform = {selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)}></PlatformSelector>
+        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}></GameGrid>
       </GridItem>
     </Grid>
   )
