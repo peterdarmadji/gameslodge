814a777b654a6520474e9a1b5604ab1fd20500cd
peterdarmadji <peterd0608@gmail.com>
2025-01-03 19:11:29 +1100

    Fixed logo margin

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = ({onSearch}: Props) => {
   return (
     // Layout items in single horizontal rows
     <HStack padding={'10px'}>
-        <Image src={logo} boxSize={'60px'} />
+        <Image src={logo} boxSize={'60px'} marginLeft={2}/>
         <SearchInput onSearch={onSearch}></SearchInput>
         <ColorModeSwitch></ColorModeSwitch>
     </HStack>
