c58e5ee8ae162e927f6cf210fdf0ea564e1ee91c
peterdarmadji <peterd0608@gmail.com>
2025-01-16 16:24:22 +0700

    Built game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
 import { Platform } from "@/entities/Platform";
+import { Genre } from "./Genre";
+import { Publisher } from "./Publisher";
 
 
 export interface Game {
   id: number;
   name: string;
+  genres: Genre[];
+  publishers: Publisher[];
   background_image: string;
   rating: number;
   parent_platforms: { platform: Platform; }[]; // an object array of type platform
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,22 +1,26 @@
-import ExpandableText from '@/components/ExpandableText'
-import useGame from '@/hooks/use-game'
-import { Heading, Spinner, Text } from '@chakra-ui/react'
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import CriticScore from "@/components/CriticScore";
+import DefinitionItem from "@/components/DefinitionItem";
+import ExpandableText from "@/components/ExpandableText";
+import GameAttributes from "@/components/GameAttributes";
+import useGame from "@/hooks/use-game";
+import { Grid, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const GameDetailPage = () => {
-  const {slug} = useParams()
-  const {data: game, error, isLoading} = useGame(slug!);
+  const { slug } = useParams();
+  const { data: game, error, isLoading } = useGame(slug!);
 
-  if (isLoading) return <Spinner />
+  if (isLoading) return <Spinner />;
   if (error || !game) throw error;
 
   return (
     <>
       <Heading>{game?.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game}></GameAttributes>
     </>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
