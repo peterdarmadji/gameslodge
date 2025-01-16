68819c32379bd63450acf3320782dc2d0470a8b0
peterdarmadji <peterd0608@gmail.com>
2025-01-16 18:56:40 +0700

    Fix search input and back to homepage

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,15 @@ import logo from '../assets/logo2.png'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
 import SettingsPage from './SettingsPage';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     // Layout items in single horizontal rows
     <HStack padding={'10px'}>
-        <Image src={logo} boxSize={'60px'} marginLeft={2}/>
+      <Link to={'/gameslodge/'}>
+        <Image src={logo} boxSize={'60px'} marginLeft={2} objectFit={'cover'}/>
+      </Link>
         <SearchInput></SearchInput>
         <SettingsPage></SettingsPage>
     </HStack>
